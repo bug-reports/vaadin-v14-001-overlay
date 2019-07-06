19925c4f4da1db3be23d81bc3edb928ab4592d62
Sven Ruppert <sven.ruppert@gmail.com>
2019-07-06 19:59:39 +0200

    init

--- src/main/java/demo/VaadinJavaApp.java
+++ src/main/java/demo/VaadinJavaApp.java
@@ -1,9 +1,13 @@
 package demo;
 
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.rapidpm.dependencies.core.logger.HasLogger;
@@ -13,20 +17,32 @@ public class VaadinJavaApp
     extends Composite<Div>
     implements HasLogger {
 
-  public static final String BTN_CLICK_ME     = "btn-click-me";
-  public static final String SPAN_CLICK_COUNT = "lb-click-count";
-
-  private final Button         btnClickMe   = new Button("click me");
-  private final Span           lbClickCount = new Span("0");
-  private final VerticalLayout layout       = new VerticalLayout(btnClickMe, lbClickCount);
+  private final Button         btn    = new Button("click me");
+  private final VerticalLayout layout = new VerticalLayout(btn);
 
   private int clickcount = 0;
 
   public VaadinJavaApp() {
-    btnClickMe.setId(BTN_CLICK_ME);
-    btnClickMe.addClickListener(event -> lbClickCount.setText(String.valueOf(++clickcount)));
 
-    lbClickCount.setId(SPAN_CLICK_COUNT);
+
+    btn.addClickListener(new ComponentEventListener<ClickEvent<Button>>() {
+      @Override
+      public void onComponentEvent(ClickEvent<Button> buttonClickEvent) {
+
+        Label      content    = new Label("Something important");
+        DatePicker datePicker = new DatePicker();
+
+        Button btnClose = new Button("Close");
+
+        VerticalLayout innerLayout  = new VerticalLayout(content, datePicker, btnClose);
+        Notification   notification = new Notification(innerLayout);
+        notification.setPosition(Notification.Position.MIDDLE);
+        notification.open();
+
+        btnClose.addClickListener(buttonClickEvent1 -> notification.close());
+      }
+    });
+
 
     logger().info("and now..  setting the main content.. ");
     getContent().add(layout);
